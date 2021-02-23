46aa5d70ddf9f42878dd9f4fb9c38e3426c3211c
Duncan Tebbs <duncan.tebbs@gmail.com>
2021-02-23 17:25:08 +0000

    scripts: helpful scripts for docker development container

---
+++ scripts/docker-init-container.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker create \
+       --name dizk \
+       --tty \
+       --interactive \
+       --volume `git rev-parse --show-toplevel`:/home/dizk \
+       --env "TERM=xterm-256color" \
+       dizk-base
---
+++ scripts/docker-make-base-image.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t dizk-base .
