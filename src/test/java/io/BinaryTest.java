127aa1764402a2d12eb13593e5ca65ed3246f826
Duncan Tebbs <duncan.tebbs@gmail.com>
2021-03-02 18:37:02 +0000

    classes for reading binary field and group data

---
+++ src/test/java/io/BinaryTest.java
@@ -0,0 +1,27 @@
+package io;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import algebra.curves.barreto_lynn_scott.bls12_377.BLS12_377Fields.BLS12_377Fr;
+import java.math.BigInteger;
+import org.junit.jupiter.api.Test;
+
+public class BinaryTest {
+
+  @Test
+  public void bignumBinaryTest() {
+    // Test expected format of bignum binary methods.
+
+    BLS12_377Fr oneFr = BLS12_377Fr.ONE;
+    BLS12_377Fr minusOneFr = BLS12_377Fr.ZERO.sub(oneFr);
+    byte[] oneFrBytes = oneFr.toBigInteger().toByteArray();
+    byte[] minusOneFrBytes = minusOneFr.toBigInteger().toByteArray();
+
+    System.out.println("Fr: oneFr      length : " + String.valueOf(oneFrBytes.length));
+    System.out.println("Fr: oneFr      bitSize: " + String.valueOf(oneFr.bitSize()));
+    System.out.println("Fr: minusOneFr length : " + String.valueOf(minusOneFrBytes.length));
+    System.out.println("Fr: minusOneFr bitSize: " + String.valueOf(minusOneFr.bitSize()));
+
+    assertEquals(new BigInteger(new byte[] {0, 1}), new BigInteger(new byte[] {1}));
+  }
+}
