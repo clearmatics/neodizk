4051e4b91cb0382d135decba89f1d69b6ef4100d
Duncan Tebbs <duncan.tebbs@gmail.com>
2021-04-09 16:32:00 +0100

    grothBGM17 proof writer implementation and tests

---
+++ src/test/java/zk_proof_systems/zkSNARK/grothBGM17/ZKSnarkObjectWriterTest.java
@@ -0,0 +1,81 @@
+package zk_proof_systems.zkSNARK.grothBGM17;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import algebra.curves.AbstractG1;
+import algebra.curves.AbstractG2;
+import algebra.curves.barreto_naehrig.bn254a.BN254aBinaryReader;
+import algebra.curves.barreto_naehrig.bn254a.BN254aBinaryWriter;
+import algebra.curves.barreto_naehrig.bn254a.BN254aFields.BN254aFr;
+import algebra.curves.barreto_naehrig.bn254a.BN254aG1;
+import algebra.curves.barreto_naehrig.bn254a.BN254aG2;
+import algebra.curves.barreto_naehrig.bn254a.bn254a_parameters.BN254aG1Parameters;
+import algebra.curves.barreto_naehrig.bn254a.bn254a_parameters.BN254aG2Parameters;
+import algebra.fields.AbstractFieldElementExpanded;
+import io.BinaryCurveReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import zk_proof_systems.zkSNARK.grothBGM17.objects.Proof;
+
+class ZKSnarkObjectWriterTest {
+
+  protected <
+          FrT extends AbstractFieldElementExpanded<FrT>,
+          G1T extends AbstractG1<G1T>,
+          G2T extends AbstractG2<G2T>>
+      Proof<G1T, G2T> dummyProof(final FrT oneFr, final G1T oneG1, final G2T oneG2) {
+    return new Proof<G1T, G2T>(
+        oneG1.mul(oneFr.construct(-3)),
+        oneG2.mul(oneFr.construct(-4)),
+        oneG1.mul(oneFr.construct(-5)));
+  }
+
+  protected <
+          FrT extends AbstractFieldElementExpanded<FrT>,
+          G1T extends AbstractG1<G1T>,
+          G2T extends AbstractG2<G2T>>
+      void writeProof(
+          final ZKSnarkObjectWriter<FrT, G1T, G2T> zkSnarkObjectWriter,
+          final FrT oneFr,
+          final G1T oneG1,
+          final G2T oneG2)
+          throws IOException {
+    zkSnarkObjectWriter.writeProof(dummyProof(oneFr, oneG1, oneG2));
+  }
+
+  protected <
+          FrT extends AbstractFieldElementExpanded<FrT>,
+          G1T extends AbstractG1<G1T>,
+          G2T extends AbstractG2<G2T>>
+      void checkProofReader(
+          final BinaryCurveReader<FrT, G1T, G2T> reader,
+          final FrT oneFr,
+          final G1T oneG1,
+          final G2T oneG2)
+          throws IOException {
+    assertEquals(
+        dummyProof(oneFr, oneG1, oneG2),
+        new Proof<G1T, G2T>(reader.readG1(), reader.readG2(), reader.readG1()));
+  }
+
+  @Test
+  public void testProofWriterBN254a() throws IOException {
+    final BN254aFr oneFr = BN254aFr.ONE;
+    final BN254aG1 oneG1 = BN254aG1Parameters.ONE;
+    final BN254aG2 oneG2 = BN254aG2Parameters.ONE;
+
+    var os = new ByteArrayOutputStream();
+    var binwriter = new BN254aBinaryWriter(os);
+    var writer = new ZKSnarkObjectWriter<BN254aFr, BN254aG1, BN254aG2>(binwriter);
+
+    writeProof(writer, oneFr, oneG1, oneG2);
+    binwriter.flush();
+    os.flush();
+    var buffer = os.toByteArray();
+
+    var reader = new BN254aBinaryReader(new ByteArrayInputStream(buffer));
+    checkProofReader(reader, oneFr, oneG1, oneG2);
+  }
+}
